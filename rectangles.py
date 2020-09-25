84534f6e79f75a1a0ae3595668395e1df62dda71
Sarah Beverton <sebryant@gmail.com>
2020-09-25 15:50:03 -0400

    changed doc string

--- rectangles.py
+++ rectangles.py
@@ -5,8 +5,8 @@ import itertools
 
 
 def main():
-    """Generates a list of tuples of (width, height) in descending
-    order based on enclosed area"""
+    """Generates a list of tuples of (width, height) from 2-9 
+    in descending order based on enclosed area"""
     one_side = [2, 3, 4, 5, 6, 7, 8, 9]
     sides = list(itertools.combinations_with_replacement(one_side, 2))
     areas = sorted(sides, key=lambda x: (x[0]-1)*(x[1]-1), reverse=True)
